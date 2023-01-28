443b52d76c17dbe637b94dc2e46b86ea9ad450ff
unknown <gamboadavid1998@gmail.com>
2023-01-27 23:53:12 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>React survey</h1>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Form>
+            <div key={`default-radio`} className="mb-1">
+              <Card.Title>Question</Card.Title>
+              <Form.Check 
+                type='radio'
+                id='default-radio'
+                name='group 1'
+                label='Opcion 1'
+              />
+              <Form.Check 
+                type='radio'
+                id='default-radio'
+                name='group 1'
+                label='Opcion 2'
+              />
+              <Form.Check 
+                type='radio'
+                id='default-radio'
+                name='group 1'
+                label='Opcion 3'
+              />
+              <Form.Check 
+                type='radio'
+                id='default-radio'
+                name='group 1'
+                label='Opcion 4'
+              />
+            </div>
+          </Form>
+        </Card.Body>
+      </Card>
+      </div>
   );
 }
 
